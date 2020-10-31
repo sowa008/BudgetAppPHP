952afb2321950dbb383edc1c353deb31268601e5
Zofia Iskierko <zofia.iskierko.programista@gmail.com>
2020-10-31 11:42:23 +0100

    Added login in php

--- register.php
+++ register.php
@@ -1,3 +1,9 @@
+<?php
+
+session_start();
+
+?>
+
 <!DOCTYPE HTML>
 <html lang="en">
     
@@ -29,14 +35,15 @@
 					
 					<div class="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3 p-4">
 						
-						<form id="form">
-							<input type="text" required placeholder="type your name" onfocus="this.placeholder=' ' " onblur="this.placeholder='type your name' ">
+						<form id="form" method="post">
+							
+							<input type="text" name="username" required placeholder="type your name" onfocus="this.placeholder=' ' " onblur="this.placeholder='type your name' ">
 			
-							<input type="email" required placeholder="type your e-mail" onfocus="this.placeholder=' ' " onblur="this.placeholder='type your e-mail' ">
+							<input type="email" name="register_email" required placeholder="type your e-mail" onfocus="this.placeholder=' ' " onblur="this.placeholder='type your e-mail' ">
 			
-							<input type="password" required placeholder="type your password" onfocus="this.placeholder=' ' " onblur="this.placeholder='type your password' ">
+							<input type="password" name="register_password" required placeholder="type your password" onfocus="this.placeholder=' ' " onblur="this.placeholder='type your password' ">
 					   
-							<input type="password" required placeholder="repeat your password" onfocus="this.placeholder=' ' " onblur="this.placeholder='repeat your password' ">
+							<input type="password" name="register_password2" required placeholder="repeat your password" onfocus="this.placeholder=' ' " onblur="this.placeholder='repeat your password' ">
 							
 							<div class="w-100"></div>
 							

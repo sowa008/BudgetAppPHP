461f79173358d79549acf763cf4bb8b15cd61a91
Zofia Iskierko <zofia.iskierko.programista@gmail.com>
2020-11-16 19:51:17 +0100

    Added SQL queries

--- showbalance.php
+++ showbalance.php
@@ -11,14 +11,16 @@
 	{
 		$user_id = $_SESSION['id'];
 		require_once 'database.php';
-			
-		$incomesQuery = $db->query("SELECT * FROM incomes WHERE user_id='$user_id'");
+		
+		$current_month=date('m');
+		$current_month_name=date("F", strtotime(date('mm')));
+					
+		$incomesQuery = $db->query("SELECT * FROM incomes, incomes_category_default WHERE incomes.user_id='$user_id' AND MONTH(incomes.date_of_income)='$current_month' AND incomes.income_category_assigned_to_user_id=incomes_category_default.id ORDER BY incomes.date_of_income");
 		$incomes = $incomesQuery->fetchAll();
 		
-		$expensesQuery = $db->query("SELECT * FROM expenses WHERE user_id='$user_id'");
-		$expenses = $expensesQuery->fetchAll();
+		$expensesQuery = $db->query("SELECT *, payment_methods_default.name AS payment, expenses_category_default.name AS category FROM expenses, expenses_category_default, payment_methods_default WHERE user_id='$user_id' AND MONTH(date_of_expense)='$current_month' AND expenses.expense_category_assigned_to_user_id=expenses_category_default.id AND payment_methods_default.id=expenses.payment_method_assigned_to_user_id ORDER BY expenses.date_of_expense");
 		
-		//print_r($incomes);
+		$expenses = $expensesQuery->fetchAll();
 		
 	}
 	
@@ -80,15 +82,15 @@
 					<div class="col-sm-12 col-md-6 col-lg-4 p-4">
 														
 							<div class="table">							
-									<table>
+									<table style="margin-left:auto; margin-right:auto;">
 										<thead>
-											<tr><th colspan="4">Together incomes: <?= $incomesQuery->rowCount() ?></th></tr>
+											<tr><th colspan="4" style="color: green;">Together incomes in <?=$current_month_name?>: <?= $incomesQuery->rowCount() ?></th></tr>
 											<tr><th>Date</th><th>Amount</th><th>Category</th><th>Comment</th></tr>
 										</thead>
 										<tbody>
 											<?php
 												foreach ($incomes as $income){
-														echo "<tr><td>{$income['date_of_income']}</td><td>{$income['amount']}</td><td>{$income['income_category_assigned_to_user_id']}</td><td>{$income['income_comment']}</td></tr>";
+														echo "<tr><td>{$income['date_of_income']}</td><td>{$income['amount']}</td><td>{$income['name']}</td><td>{$income['income_comment']}</td></tr>";
 												}
 											?>	
 										</tbody>
@@ -96,16 +98,25 @@
 							</div>
 							
 							<div class="table">							
-									<table>
+									<table style="margin-left:auto; margin-right:auto;">
 										<thead>
-											<tr><th colspan="5">Together expenses: <?= $expensesQuery->rowCount() ?></th></tr>
+											<tr><th colspan="5" style="color: red;">Together expenses in <?=$current_month_name?>: <?= $expensesQuery->rowCount() ?></th></tr>
 											<tr><th>Date</th><th>Amount</th><th>Category</th><th>Payment</th><th>Comment</th></tr>
 										</thead>
 										<tbody>
 											<?php
 												foreach ($expenses as $expense){
-														echo "<tr><td>{$expense['date_of_expense']}</td><td>{$expense['amount']}</td><td>{$expense['payment_method_assigned_to_user_id']}</td><td>{$expense['expense_category_assigned_to_user_id']}</td><td>{$expense['expense_comment']}</td></tr>";
+														echo "
+														<tr>
+														<td>{$expense['date_of_expense']}</td>
+														<td>{$expense['amount']}</td>
+														<td>{$expense['category']}</td>
+														<td>{$expense['payment']}</td>
+														<td>{$expense['expense_comment']}</td>
+														</tr>
+														";
 												}
+
 											?>	
 										</tbody>
 									</table>
